0ff07e84b6373c3c8c4a005a77b1d2d200616086
wilson <wilson@wilsondeAir.lan>
2018-08-27 23:34:29 +0800

    增加直播列表接口

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,6 +6,7 @@ use App\LiveList;
 use App\Room;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class MainController extends Controller
 {
@@ -20,6 +21,22 @@ class MainController extends Controller
 
         $data['site'] = $this->site;
         return view('mobile.index', $data);
+    }
+
+
+    public function livelist()
+    {
+        $livelists = LiveList::with(['room'])->where('end_time', '>', date('Y-m-d H:i:s'))
+            ->orderBy('start_time', 'asc')->get();
+
+        $livelists->transform(function ($item, $key){
+            $item->teacher = $item->room ? $item->room->teacher->name : '';
+            $item->image = Storage::disk('uploads')->url($item->image);
+            $item->url = route('room.index', ['id'=>$item->room_id]);
+            return $item->only(['id','teacher', 'url', 'image', 'start_time', 'end_time', 'name']);
+        });
+
+        return response()->json($livelists);
     }
 
 
